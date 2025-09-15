8dc506bae77ccb60004e429d32c13b8a33ac424f
= <danishho246@gmail.com>
2025-09-16 01:49:34 +0800

    done 10,11

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = n => {
+ 
+    
+    if(n % 4 == 0 || n % 400 == 0){
+        if(n % 100 == 0){
+            if(n % 400 == 0) return true
+            return false
+        }
 
-};
+        return true
+    }
+
+    return false
+}
 
 // Do not edit below this line
 module.exports = leapYears;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,5 @@
-const convertToCelsius = function() {
-};
-
-const convertToFahrenheit = function() {
-};
+const convertToCelsius = f => Math.round( ((f - 32) * 5/9) * 10) /10;
+const convertToFahrenheit = c => Math.round(((c * 9 / 5) + 32) * 10) /10;
 
 // Do not edit below this line
 module.exports = {
