fc34f909cb2a39d40be671117a929e3e3957579a
= <danishho246@gmail.com>
2025-09-19 18:21:11 +0800

    done 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = (arr) => { const oldest = arr.reduce((a,b) => {
+    const ageA = (a.yearOfDeath || 2025)- a.yearOfBirth;
+    const ageB = (b.yearOfDeath || 2025) - b.yearOfBirth; 
+    const higher = ageA > ageB ? a : b;
+    return higher
+})
+return oldest
+}
 // Do not edit below this line
 module.exports = findTheOldest;
