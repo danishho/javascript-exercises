74dcd4191e6d569173a88c60224d7204f075625f
= <danishho246@gmail.com>
2025-09-19 15:20:27 +0800

    done 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.trim().toLowerCase().replace(/[^a-zA-Z0-9]/g, '')
+;
+    console.log(str);
+    let j = str.length - 1
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] != str[j]) {
+            return false;
+        }
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
