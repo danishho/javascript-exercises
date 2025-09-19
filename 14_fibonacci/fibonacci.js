af6b752b2955415eb53cc63e792d16c5c1100acf
= <danishho246@gmail.com>
2025-09-19 17:49:20 +0800

    14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(b) {
+    n = parseInt(b);
+    if(typeof n != "number" || n < 0 ){
+        return ("OOPS")
+    }
+    if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= n; i++) {
+      let temp = a + b;
+      a = b;
+      b = temp;
+    }
+    return b;
+  }
 };
 
 // Do not edit below this line
